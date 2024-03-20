444893690039b2acf3216f987149cbccc040976b
FelipeCarvalho <101579628+Felipe-Araujo-duck@users.noreply.github.com>
2024-03-19 21:53:21 -0300

    Adicionar arquivos de projeto.

---
+++ Strong Fit/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Strong_Fit.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//Configuração da Entity Framework Core
+
+builder.Services.AddDbContext<Context>(options =>
+
+options.UseSqlServer(builder.Configuration["Data:Strong_Fit:ConnectionString"]));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
